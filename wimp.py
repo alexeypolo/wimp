d7c9dfd7b75424af4a9b9986247cafce1ce0158a
Alexey Polonsky <alexey.polonsky@intel.com>
2015-11-30 17:46:15 +0200

    added description, a story about JSON and two examples

--- wimp.py
+++ wimp.py
@@ -6,19 +6,25 @@ import sys
 import optparse
 
 def cl_parse(argv):
+    description="WIMP (Where Is My Password) password manager. \
+Supports multiple tags, passwords history, AES encryption. \
+New passwords are read from stdin. \
+JSON is used for all options that take complex arguments. \
+PASSWORD is formatted as a single JSON object, TAGS is formatted as a JSON list of strings.\
+"
     usage="python %prog [options]"
     epilog='''
-Example 1:
-    TODO
+Example 1: Add new password
+    python wimp.py --add="{'title':'stackoverflow', 'username':'my_user_name', 'url'='http://stackoverflow.com/', 'tags':'fun'}"
 
-Example 2:
-    TODO
+Example 2: List all passwords tagged as 'fun' and/or 'work'
+    python wimp.py --list_by_tags="['fun','work']"
 '''
 
     # dont strip new-lines from multiline epilog but just print it as-is
     optparse.OptionParser.format_epilog = lambda self, formatter: self.epilog
 
-    parser = optparse.OptionParser(usage=usage, version="%prog 0.1", epilog=epilog)
+    parser = optparse.OptionParser(description=description, usage=usage, version="%prog 0.1", epilog=epilog)
 
     group = optparse.OptionGroup(parser, "Basics", "")
     group.add_option("-a", "--add", dest="add_password", metavar="PASSWORD",
@@ -36,6 +42,8 @@ Example 2:
         help="List passwords filtered by a list of tags")
     group.add_option("--list_tags", dest="list_tags", metavar="[TAGS]",
         help="List tags")
+    group.add_option("--echo", dest="echo_password", action="store_true",
+        help="Echo when typing a new password. By default this option is 'off'.")
     parser.add_option_group(group)
 
     (options, left_over_args) = parser.parse_args(argv)
